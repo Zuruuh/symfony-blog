4d19c7ec59b4fddb309dfcfcd66f602805ca20b2
Zuruh <ziadi.mail.pro@gmail.com>
2022-06-11 02:38:31 +0200

    🐛 Fixed config for prod

--- symfony-blog-server/config/packages/debug.php
+++ symfony-blog-server/config/packages/debug.php
@@ -2,10 +2,14 @@
 
 use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Config\DebugConfig;
 
-return static function (DebugConfig $debug, ContainerConfigurator $container) {
-    if ($container->env() === 'dev') {
-        $debug->dumpDestination('tcp://' . env('VAR_DUMPER_SERVER'));
-    }
+if (class_exists('\Symfony\Config\DebugConfig')) {
+    return static function (\Symfony\Config\DebugConfig $debug, ContainerConfigurator $container) {
+        if ($container->env() === 'dev') {
+            $debug->dumpDestination('tcp://' . env('VAR_DUMPER_SERVER'));
+        }
+    };
+}
+
+return static function () {
 };
--- symfony-blog-server/config/packages/web_profiler.php
+++ symfony-blog-server/config/packages/web_profiler.php
@@ -4,16 +4,21 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use Symfony\Config\WebProfilerConfig;
 use Symfony\Config\FrameworkConfig;
 
-return static function (WebProfilerConfig $profiler, FrameworkConfig $framework,ContainerConfigurator $container) {
-    if ($container->env() === 'dev') {
-        $profiler->toolbar(true);
-        $profiler->interceptRedirects(false);
-        $framework->profiler()->onlyExceptions(false);
-    }
+if (class_exists('\Symfony\Config\WebProfilerConfig')) {
+    return static function (WebProfilerConfig $profiler, FrameworkConfig $framework, ContainerConfigurator $container) {
+        if ($container->env() === 'dev') {
+            $profiler->toolbar(true);
+            $profiler->interceptRedirects(false);
+            $framework->profiler()->onlyExceptions(false);
+        }
 
-    if ($container->env() === 'test') {
-        $profiler->toolbar(false);
-        $profiler->interceptRedirects(false);
-        $framework->profiler()->collect(false);
-    }
+        if ($container->env() === 'test') {
+            $profiler->toolbar(false);
+            $profiler->interceptRedirects(false);
+            $framework->profiler()->collect(false);
+        }
+    };
+}
+
+return static function () {
 };
