87cb37004d99810ffd86e5e98c9ee0213c2722f6
Zuruuh <ziadi.mail.pro@gmail.com>
2023-08-04 23:49:38 +0200

    Test CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
 
+fakeFunctionJustToTestCI();
+
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
